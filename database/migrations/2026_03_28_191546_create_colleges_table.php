e75c82a8d22ec0195b006fb0cf54ac721236789c
Jjustin91 <justinianijonathan91@gmail.com>
2026-03-29 03:42:47 +0800

    Initial Commit

---
+++ database/migrations/2026_03_28_191546_create_colleges_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('colleges', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique(); // e.g., College of Information Technology and Computing
+            $table->string('abbreviation')->unique(); // e.g., CITC
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('colleges');
+    }
+};
